ca2710e0e01f89c06a720ae0b3517b736ff6d2fa
millardjimmy <millardjimmy@gmail.com>
2020-08-03 01:50:03 -0600

    schema reflect UID and Provider

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_004922) do
+ActiveRecord::Schema.define(version: 202008032212401) do
 
   create_table "categories", force: :cascade do |t|
-    t.string "category_name"
-    t.string "category_desc"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -22,53 +21,36 @@ ActiveRecord::Schema.define(version: 2020_08_03_004922) do
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.integer "user_id"
-    t.integer "task_id"
+    t.integer "question_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["task_id"], name: "index_comments_on_task_id"
+    t.index ["question_id"], name: "index_comments_on_question_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "lists", force: :cascade do |t|
-    t.string "list_name"
-    t.string "list_desc"
-    t.integer "user_id", null: false
-    t.integer "category_id", null: false
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.integer "user_id"
+    t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_lists_on_category_id"
-    t.index ["user_id"], name: "index_lists_on_user_id"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "tag_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "task_dec"
-    t.string "task_priority"
-    t.integer "list_id", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["list_id"], name: "index_tasks_on_list_id"
-    t.index ["user_id"], name: "index_tasks_on_user_id"
+    t.index ["category_id"], name: "index_questions_on_category_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
+    t.string "provider"
+    t.string "email"
   end
 
-  add_foreign_key "comments", "tasks"
+  add_foreign_key "comments", "questions"
   add_foreign_key "comments", "users"
-  add_foreign_key "lists", "categories"
-  add_foreign_key "lists", "users"
-  add_foreign_key "tasks", "lists"
-  add_foreign_key "tasks", "users"
+  add_foreign_key "questions", "categories"
+  add_foreign_key "questions", "users"
 end
