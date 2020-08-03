24420a09790b012597f20e56d4c41a125ae551c3
millardjimmy <millardjimmy@gmail.com>
2020-08-02 19:52:25 -0600

    route remaster

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,28 @@
 Rails.application.routes.draw do
-  resources :tasks
-  resources :tags
-  resources :cotegories
-  resources :comments
-  resources :users
-  resources :lists
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+  resources :users, only: [] do
+    resources :categories, only: [:new, :edit, :create, :update, :destroy, :show] do
+      resources :lists, only: [:new, :edit, :create, :update, :destroy, :show] do
+        resources :tasks, only: [:new, :edit, :create, :update, :destroy]
+
+
+      end
+    end
+  end
+  
+  # resources :tasks
+  # resources :tags
+  # resources :cotegories
+  # resources :comments
+  # resources :users
+  # resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/users/:id', to: 'application#show', as: 'user'
+  # root 'application#hello'
 end
