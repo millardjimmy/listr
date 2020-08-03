a05ac2fe1152b53f9203ecdce093b1359a733168
millardjimmy <millardjimmy@gmail.com>
2020-08-03 03:13:55 -0600

    most popular cat - needs attention

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
     has_many :questions
     validates :name, presence: :true 
     validates_uniqueness_of :name, message: "that category has already been created."
+
+    def self.most_popular_cat
+        self.all.sort_by {|cat| cat.category.count}
+    end
 end
