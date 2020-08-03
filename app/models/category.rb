176112667258ac9b6526af1523f275e6b0d5e843
millardjimmy <millardjimmy@gmail.com>
2020-08-03 16:15:11 -0600

    aspirational pseudocode

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,9 @@ class Category < ApplicationRecord
     validates :name, presence: :true 
     validates_uniqueness_of :name, message: "that category has already been created."
 
+#write in SQL term - join with questions table ORDER|GROUP|COUNT
     def self.most_popular_cat
         self.all.sort_by {|cat| cat.category.count}
     end
+    
 end
