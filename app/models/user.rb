72892bffb55d7a9d793299420751d70c1a295e8c
millardjimmy <millardjimmy@gmail.com>
2020-08-02 19:52:10 -0600

    validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :categories
     has_many :lists, through: :categories
     has_many :tasks, through: :lists
+    has_secure_password
+
+    validates :name, presence: true
+    validates :username, presence: true
+    validates :username, uniqueness: true
 end
