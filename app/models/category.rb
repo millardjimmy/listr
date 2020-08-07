d576d454258c24a8f4c588c0c023f30f38d92aa5
millardjimmy <millardjimmy@gmail.com>
2020-08-07 09:42:12 -0600

    added depency destruction

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,8 @@
 class Category < ApplicationRecord
-    has_many :questions
+    has_many :questions, dependent: :destroy
     validates :name, presence: :true 
     validates_uniqueness_of :name, message: "that category has already been created."
 
-#write in SQL term - join with questions table ORDER|GROUP|COUNT
-    def self.most_popular_cat
-        self.all.sort_by {|cat| cat.category.count}
-    end
+
     
 end
